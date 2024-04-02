6952f37f7f020f92594f98f0d97290da4cf4cbb2
vats-tushar <121571005+vats-tushar@users.noreply.github.com>
2024-04-02 21:29:07 +0530

    first

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 # 3rd parth dependencies
-from waitress import serve
 from flask import Flask
 from deepface.api.src.modules.core.routes import blueprint
 
@@ -9,4 +8,3 @@ def create_app():
     return app
 deepface_app = create_app()
 
-serve(deepface_app, host='0.0.0.0', port=5000, threads=2)
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from app import deepface_app
+
+if __name__ == '__main__':
+    deepface_app.run(debug=True)
